66d225328ab165718fd56c97a7e1dfee78f588a5
PeterAkaJohn <pierpaolo.iannone93@gmail.com>
2024-04-08 21:47:02 +0200

    ref: move date string parser in its own method, add basic validation and tests

--- file_processing/src/dataframe/datetime.rs
+++ file_processing/src/dataframe/datetime.rs
@@ -1,12 +1,32 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use polars::lazy::dsl::{datetime, lit, DatetimeArgs, Expr};
 
-pub fn create_datetime(value: &str) -> Result<Expr> {
+pub fn to_datetime_expression(value: &str) -> Result<Expr> {
+    let DateParts(year, month, day, hour, minute, second): DateParts = extract_date_parts(value)?;
+
+    Ok(datetime(
+        DatetimeArgs::new(lit(year), lit(month), lit(day)).with_hms(
+            lit(hour),
+            lit(minute),
+            lit(second),
+        ),
+    ))
+}
+
+struct DateParts(i32, i32, i32, i32, i32, i32);
+
+fn extract_date_parts(value: &str) -> Result<DateParts> {
     let (date_parts, time_parts) = value
         .split_once(" ")
         .ok_or_else(|| anyhow::anyhow!("Invalid datetime format"))?;
     let date_parts: Vec<&str> = date_parts.split("-").collect();
     let time_parts: Vec<&str> = time_parts.split(":").collect();
+
+    if date_parts.len() != 3 || time_parts.len() != 3 {
+        return Err(anyhow!(
+            "Wrong datetime format. Needs to be 'YYYY-MM-DD hh-mm-ss'"
+        ));
+    }
 
     let year = date_parts[0]
         .parse::<i32>()
@@ -26,12 +46,33 @@ pub fn create_datetime(value: &str) -> Result<Expr> {
     let second = time_parts[2]
         .parse::<i32>()
         .map_err(|_| anyhow::anyhow!("Failed to parse second"))?;
-
-    Ok(datetime(
-        DatetimeArgs::new(lit(year), lit(month), lit(day)).with_hms(
-            lit(hour),
-            lit(minute),
-            lit(second),
-        ),
-    ))
+    Ok(DateParts(year, month, day, hour, minute, second))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn check_extract_date_parts() {
+        let DateParts(year, month, day, hour, minute, second) =
+            extract_date_parts("2024-02-01 17:02:00").expect("Value should be parsed correctly");
+    
+        assert_eq!(year, 2024);
+        assert_eq!(month, 02);
+        assert_eq!(day, 01);
+        assert_eq!(hour, 17);
+        assert_eq!(minute, 02);
+        assert_eq!(second, 00);
+    }
+    
+    #[test]
+    fn check_extract_date_parts_throws() {
+        assert!(extract_date_parts("2024-01 17:02:00").is_err());
+        assert!(extract_date_parts("2024-02-01 sadsd:02:00").is_err());
+        assert!(extract_date_parts("2024-01-01 17:02").is_err());
+        assert!(extract_date_parts("2024-01-01 17:02:").is_err());
+        assert!(extract_date_parts("2024-01 17:02").is_err());
+        assert!(extract_date_parts("2024-01 17:02:").is_err());
+    }
+}
+
--- file_processing/src/dataframe/expressions.rs
+++ file_processing/src/dataframe/expressions.rs
@@ -2,7 +2,7 @@ use anyhow::Context;
 use polars::lazy::dsl::Expr;
 use polars::lazy::dsl::col;
 
-use super::datetime::create_datetime;
+use super::datetime::to_datetime_expression;
 
 pub fn get_index_expr_if_needed(
     index_name: &Option<String>,
@@ -10,7 +10,7 @@ pub fn get_index_expr_if_needed(
 ) -> Option<polars::lazy::dsl::Expr> {
     return match (index_name, index_value) {
         (Some(index_n), Some(index_val)) => {
-            let index_value_dt: Expr = create_datetime(&index_val)
+            let index_value_dt: Expr = to_datetime_expression(&index_val)
                 .with_context(|| "Failed to format index-value")
                 .unwrap();
             let index_expr = col(&index_n).eq(index_value_dt);
