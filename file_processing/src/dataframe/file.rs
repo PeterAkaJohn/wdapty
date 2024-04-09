011180a9840a440d4818b0a8610536c2124e0fe6
PeterAkaJohn <pierpaolo.iannone93@gmail.com>
2024-04-09 17:36:52 +0200

    chore(file_processing): add test for file mod

--- file_processing/src/dataframe/file.rs
+++ file_processing/src/dataframe/file.rs
@@ -14,3 +14,43 @@ pub fn handle_output(output_file: Option<String>, mut df: polars::prelude::DataF
         println!("{}", df);
     })
 }
+
+
+#[cfg(test)]
+mod test {
+    use polars::{frame::DataFrame, prelude::NamedFrom, series::Series};
+
+    use crate::generated_test_files_path;
+
+    use super::*;
+
+    #[test]
+    fn test_handle_output_success_with_output_file() {
+        let series = (0..10).map(|col_name| Series::new(&col_name.to_string(), &vec!["A", "B", "C"])).collect::<Vec<Series>>();
+        let df = DataFrame::new(series);
+        assert!(df.is_ok());
+        let df = df.unwrap();
+        let result = handle_output(Some(generated_test_files_path!("test_handle_output.parq")), df);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_handle_output_success_without_output_file() {
+        let series = (0..10).map(|col_name| Series::new(&col_name.to_string(), &vec!["A", "B", "C"])).collect::<Vec<Series>>();
+        let df = DataFrame::new(series);
+        assert!(df.is_ok());
+        let df = df.unwrap();
+        let result = handle_output(None, df);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_handle_output_failure_path_does_not_exist() {
+        let series = (0..10).map(|col_name| Series::new(&col_name.to_string(), &vec!["A", "B", "C"])).collect::<Vec<Series>>();
+        let df = DataFrame::new(series);
+        assert!(df.is_ok());
+        let df = df.unwrap();
+        let result = handle_output(Some(generated_test_files_path!("/do/not/exist/test_handle_output.parq")), df);
+        assert!(result.is_err());
+    }
+}
